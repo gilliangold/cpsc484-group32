19bc807fe0a5e0fe16e21c18ded032e15db5e7ea
Shiqi <2412230983lsq@gmail.com>
2023-04-11 21:46:58 -0400

    add gitignore

---
+++ static/js/exercise.js
@@ -0,0 +1,20 @@
+var host = "cpsc484-01.yale.internal:8888";
+$(document).ready(function() {
+  twod.start();
+});
+
+var twod = {
+  socket: null,
+
+  start: function() {
+    var url = "ws://" + host + "/twod";
+    twod.socket = new WebSocket(url);
+    twod.socket.onmessage = function(event) {
+      twod.show(JSON.parse(event.data));
+    }
+  },
+
+  show: function(twod) {
+    $('.twod').attr("src", 'data:image/pnjpegg;base64,'+twod.src);
+  }
+};
